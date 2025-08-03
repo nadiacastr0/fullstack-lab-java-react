f9abab03eac074b0218de5f7abd50d97a72bf7d3
nadiacastr0 <nadianaely32@gmail.com>
2025-08-03 17:57:09 -0300

    Initial project setup: added backend and frontend structure

---
+++ cardapio-back-end/src/main/java/com/example/cardapio_back_end/CardapioBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardapio_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioBackEndApplication.class, args);
+	}
+
+}
---
+++ cardapio-back-end/src/test/java/com/example/cardapio_back_end/CardapioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardapio_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
